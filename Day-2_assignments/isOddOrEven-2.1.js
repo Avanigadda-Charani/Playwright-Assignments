944ac98bdba993220d104945d9a5af486049af47
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 00:40:08 +0530

    Update isOddOrEven-2.1.js

--- Day-2_assignments/isOddOrEven-2.1.js
+++ Day-2_assignments/isOddOrEven-2.1.js
@@ -1,10 +1,10 @@
-function isOddOrEven(num){
-    
-    if(num%2==0){
-        console.log("even")
+function isOddOrEven(num) {
+
+    if (num % 2 === 0) {
+        console.log("even")  //number is divisible by 2 with reminder zero then its even number
     }
-    else{
-        console.log("odd")
+    else {
+        console.log("odd") //number not divisible by 2 with remainder one then its odd number
     }
 
 }
