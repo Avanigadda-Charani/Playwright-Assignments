90ae02f0f63d0bb5b94af22df7eb73e543a21219
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 00:28:01 +0530

    Update GradeCalculation-2.5.js

--- Day-2_assignments/GradeCalculation-2.5.js
+++ Day-2_assignments/GradeCalculation-2.5.js
@@ -1,18 +1,13 @@
-function studentsScore(score){
+function studentsScore(score) {
     switch (true) {
-        case score>=90:
-        return "A Grade";
-            break;
-        case score>=75:
-        return "B Grade";
-            break;
-        case score>=50:
-        return "C Grade";
-            break;
-    
+        case score >= 90:  // Score 90 or above gets an A Grade
+            return "A Grade";
+        case score >= 75:   // Score 75 or above gets an B Grade
+            return "B Grade";
+         case score >= 50:   // Score 50 or above gets an C Grade
+            return "C Grade";
         default:
-        console.log("Fail");
-            break;
+            console.log("Fail");  //Score less than 50 will fail
     }
 }
 console.log(studentsScore(80))
