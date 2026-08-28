79d3714810aabb8b6763abc5a69e2e0f408f07a9
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 00:35:01 +0530

    Update NumberType-2.2.js

--- Day-2_assignments/NumberType-2.2.js
+++ Day-2_assignments/NumberType-2.2.js
@@ -1,15 +1,15 @@
-function integers(){
-    let num=-89
-    if(num>0){
-        console.log("it is a positive number")
+function checkNumberType() {
+    let num = -89
+    if (num > 0) {
+        console.log("it is a positive number") //if number is grater than zero then its positive
     }
-    else if(num<0){
-        console.log("it is a negative number")
+    else if (num < 0) {
+        console.log("it is a negative number") //if number is less than zero then its negative
     }
-    else{
-        console.log("it is a neutral number")
+    else {
+        console.log("it is a neutral number") // number equals to zero then its neutral number
 
-    }  
+    }
 
 }
-integers()
+checkNumberType()
