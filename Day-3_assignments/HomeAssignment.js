53e70966ce8cc7a6c618784713093e75e830541f
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 00:51:35 +0530

    Update HomeAssignment.js

--- Day-3_assignments/HomeAssignment.js
+++ Day-3_assignments/HomeAssignment.js
@@ -3,15 +3,15 @@
 // Input: s = "Hello World" 
 // Output: 5 
 // Explanation: The last word is "World" with length 5. 
-let Str1="nice day"
-console.log(Str1.slice(5))
+let str1 = "nice day"
+console.log(str1.slice(5))
 // Example 2:  
 // Input: s = "   fly me   to   the moon  " 
 // Output: 4 
 // Explanation: The last word is "moon" with length 4. 
-let Str2=" have a nice day "
-let output=Str2.trim().split(" ")
-console.log(output[output.length-1].length)
+let str2 = " have a nice day "
+let output = str2.trim().split(" ")
+console.log(output[output.length - 1].length)
 // Example 3:  
 // Write a function to check if two strings are anagrams. 
 //  Input: isAnagram('listen', 'silent') 
@@ -21,15 +21,15 @@ console.log(output[output.length-1].length)
 //  Explanation: An anagram is when you mix up the letters of a word to make a new one, using all 
 // the letters. 
 
-function isAnagram(stri1,stri2){
-let sort1=stri1.split("").sort().join("")
-let sort2=stri2.split("").sort().join("")
-if(sort1===sort2){
-    console.log("true");   
-}else{
-    console.log("false");
-    
+function isAnagram(string1, string2) {
+    let sortedStr1 = string1.split("").sort().join("")
+    let sortedStr2 = string2.split("").sort().join("")
+    if (sortedStr1 === sortedStr2) {
+        return true
+    } else {
+        return false
+
+    }
 }
-}
- isAnagram('listen','silent') 
- isAnagram('cat','rat')
+console.log(isAnagram("listen", "silent"))
+console.log(isAnagram("cat", "rat"))
