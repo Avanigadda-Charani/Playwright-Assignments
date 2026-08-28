5fbe6d2b7e527adb3cea96bbc5ad04a9f8824473
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 01:00:21 +0530

    Update Intersection.js

--- Day-3_assignments/Intersection.js
+++ Day-3_assignments/Intersection.js
@@ -1,17 +1,19 @@
-let unique=[]
-function intersection(arr1,arr2){
-    for(let i=0;i<arr1.length;i++){
-        if(arr2.includes(arr1[i])){
-            if(!unique.includes(arr1[i])){
-                unique.push(arr1[i])
+function intersection(arr1, arr2) {
+     let uniqueValues = []
+    for (let i = 0; i < arr1.length; i++) {
+        
+        if (arr2.includes(arr1[i])) {
+           
+            if (!uniqueValues.includes(arr1[i])) { //includes find common elements and avoid duplicates
+                uniqueValues.push(arr1[i])
 
             }
         }
-      
+
     }
-    return unique
+    return uniqueValues
 
 }
-console.log(intersection([1,4,3,2,4],[6,3,2,1,8]))
+console.log(intersection([1, 4, 3, 2, 4], [6, 3, 2, 1, 8]))
 console.log(intersection([1, 9, 3], [1, 8, 9]));
 console.log(intersection([4, 0, 2, 3], [2, 0, 3, 4]));
