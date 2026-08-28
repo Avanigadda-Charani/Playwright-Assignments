724f5d5281ee47e1d62bb293245f67a7767e73f0
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 01:11:19 +0530

    Update TwoSum.js

--- Day-3_assignments/TwoSum.js
+++ Day-3_assignments/TwoSum.js
@@ -1,16 +1,18 @@
 const nums = [2, 4, 7, 8, 11, 14];
-let result=[]
+let resultIndexes = [];
 let target = 18;
 
 for (let i = 0; i < nums.length; i++) {
-    for(let j=i+1;j<nums.length;j++){
-         if (nums[i] + nums[j] === target) {
+
+    // Start j from i + 1 to avoid comparing an element with itself and to avoid checking the same pair again.
+    for (let j = i + 1; j < nums.length; j++) {
+
+        // Checking whether the two numbers add up to the target.
+        if (nums[i] + nums[j] === target) {
             console.log(true);
-            result.push([i,j])
-
+            resultIndexes.push([i, j]);
         }
-
-   
     }
 }
-console.log(result)
+
+console.log(resultIndexes);
