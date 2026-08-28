cf01c5bd6006bfee78e50d46535444e346eeefe3
Avanigadda-Charani <avanigadda.charani@gmail.com>
2026-08-29 01:19:43 +0530

    Update RedBus.spec.ts

--- DAY-4_assignments/RedBus.spec.ts
+++ DAY-4_assignments/RedBus.spec.ts
@@ -1,21 +1,39 @@
- import {chromium,firefox,test} from "@playwright/test"
-test("Redbus browser launch",async()=>{
-    const browser1=await firefox.launch({headless:false,channel:"firefox"})
-    const browser2=await chromium.launch({headless:false,channel:"msedge"})
-    const context1=await browser1.newContext()
-    const context2=await browser2.newContext()
-    const page1=await context1.newPage()
-    const page2=await context2.newPage()
-    //Flipcart
-    await page1.goto("https://www.flipkart.com")
-    console.log(await page1.title())
-    console.log(page1.url())
-    //RedBus
-    await page2.goto("https://www.redbus.in")
-    console.log(await page2.title())
-    console.log(page2.url())
-    //time to see opening of browser clearly
-    await page1.waitForTimeout(5000) 
-    await page2.waitForTimeout(5000) 
-})
+import { chromium, firefox, test } from "@playwright/test";
+
+test("Launch Firefox and Edge browsers", async () => {
+
+    const firefoxBrowser = await firefox.launch({
+        headless: false,
+        channel: "firefox"
+    });
+
+    const edgeBrowser = await chromium.launch({
+        headless: false,
+        channel: "msedge"
+    });
+
+    const firefoxContext = await firefoxBrowser.newContext();
+    const edgeContext = await edgeBrowser.newContext();
+
+    const firefoxPage = await firefoxContext.newPage();
+    const edgePage = await edgeContext.newPage();
+
+    // Flipkart
+    await firefoxPage.goto("https://www.flipkart.com");
+    console.log(await firefoxPage.title());
+    console.log(firefoxPage.url());
+
+    // RedBus
+    await edgePage.goto("https://www.redbus.in");
+    console.log(await edgePage.title());
+    console.log(edgePage.url());
+
+    // Wait to clearly see the browsers opening
+    await firefoxPage.waitForTimeout(5000);
+    await edgePage.waitForTimeout(5000);
+
+    // Close the browsers after execution
+    await firefoxBrowser.close();
+    await edgeBrowser.close();
+});
 
